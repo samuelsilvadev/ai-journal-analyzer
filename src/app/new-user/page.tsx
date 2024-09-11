18d0519068045a8d1301a06cdbe4bde94ce3653b
Samuel Silva <samuelsilvawb@gmail.com>
2024-09-11 21:56:50 +0100

    feat: setup authentication with clerk

---
+++ src/app/new-user/page.tsx
@@ -0,0 +1,9 @@
+function NewUserPage() {
+  return (
+    <div>
+      <h1>New User</h1>
+    </div>
+  );
+}
+
+export default NewUserPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
+      <div className="flex flex-col gap-5 max-w-[600px]">
+        <h1 className="text-6xl">Hey</h1>
+        <p className="text-2xl text-white/60">
+          Join us and start your journey to track your daily activities and stay
+          connected with your inner self.
+        </p>
+        <button className="bg-blue-600 px-4 py-2 rounded-lg text-lg self-start">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
